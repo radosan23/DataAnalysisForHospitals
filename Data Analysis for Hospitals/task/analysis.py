9dbc64a4102154535114cd6f7e56a275e28227ec
radosan <radoslaw.sanetra@gmail.com>
2023-12-08 21:17:41 +0100

    merged datasets, unified labels

--- Data Analysis for Hospitals/task/analysis.py
+++ Data Analysis for Hospitals/task/analysis.py
@@ -4,12 +4,13 @@ import pandas as pd
 def main():
     pd.set_option('display.max_column', 8)
 
-    general = pd.read_csv('test/data/general.csv')
-    prenatal = pd.read_csv('test/data/prenatal.csv')
-    sports = pd.read_csv('test/data/sports.csv')
+    general = pd.read_csv('test/data/general.csv', index_col=0)
+    prenatal = pd.read_csv('test/data/prenatal.csv', index_col=0)
+    sports = pd.read_csv('test/data/sports.csv', index_col=0)
 
-    for df in [general, prenatal, sports]:
-        print(df.head(20))
+    prenatal.columns = sports.columns = general.columns
+    df = pd.concat([general, prenatal, sports], ignore_index=True)
+    print(df.sample(n=20, random_state=30))
 
 
 if __name__ == '__main__':
